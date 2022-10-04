a9192831a464b77637e6140e28ce1c0170b01073
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-04 13:46:55 -0500

    Add test for Task 1.2

--- __test__/stringLength.test.js
+++ __test__/stringLength.test.js
@@ -11,4 +11,26 @@ describe("string tests", () => {
     //Assert
     expect(result1).toBe(5);
   });
-});  
+
+  test ("string validation NotNull", () => {
+    //Arrange
+    const string2 ="";
+    
+    //Act
+    const result2 = 'String is empty'
+    
+    //Assert
+    expect(result2).toBe(result2);
+  });
+
+  test ("string validation NotTooLong", () => {
+    //Arrange
+    const string3 ="Australopitecus";
+    
+    //Act
+    const result3 = 'String is too long'
+    
+    //Assert
+    expect(result3).toBe(result3);
+  });
+});
--- src/modules/stringLength.js
+++ src/modules/stringLength.js
@@ -1,5 +1,19 @@
 function stringLength(string) {
+  try {
+    if (string.length < 1) {
+      throw new Error('String is empty');
+    }
+    if (string.length > 10) {
+      throw new Error('String is too long');
+    }
+  }
+  catch (e) {
+    return e.message;
+  }
+
   return string.length;
 };
+
+
 
 module.exports = stringLength;
